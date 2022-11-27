84062f0fffe9500d75a2a62685416a70aea5d197
Limpsobaka <limpsobaka@yandex.ru>
2022-11-27 13:33:51 +0300

    Change main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
   public static void main(String[] args) throws InterruptedException {
+    int threadCount = 4;
+
     System.out.println("Создаю потоки...");
     ThreadGroup tg = new ThreadGroup("tg");
-    for (int i = 1; i < 5; i++) {
+    for (int i = 1; i <= threadCount; i++) {
       new MyThread(tg, i + "").start();
     }
     Thread.sleep(15000);
